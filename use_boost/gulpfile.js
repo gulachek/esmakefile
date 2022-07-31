23c97eb47c208a08a805fdf87f2f12e3336ac427
Nicholas Gulachek <nsgulachek@gmail.com>
2022-07-31 18:36:01 -0500

    Add gcc toolchain support

--- use_boost/gulpfile.js
+++ use_boost/gulpfile.js
@@ -6,7 +6,7 @@ const sys = new BuildSystem(__dirname);
 const cpp = new Cpp(sys);
 
 const boost = {};
-boost.log = cpp.require('org.boost.log', '1.78.0');
+boost.log = cpp.require('org.boost.log', '1.74.0');
 
 const foo = cpp.library('com.example.foo', '0.1.0',
 	'src/foo.cpp'

d452193fe3db739a885af97d6c422c5099936ad4
Nicholas Gulachek <gulac004@umn.edu>
2023-08-17 21:03:46 -0500

    refactor Build into file

---
+++ src/Build.ts
@@ -0,0 +1,107 @@
+import { IBuildPath, Path } from './Path';
+
+import { readFile, writeFile, mkdir } from 'node:fs/promises';
+import { dirname } from 'node:path';
+
+export type RecipeID = number;
+
+export function isRecipeID(id: any): id is RecipeID {
+	return typeof id === 'number';
+}
+
+export type RecipeInfo = {
+	buildAsync(): Promise<boolean>;
+	sources: Path[];
+	targets: IBuildPath[];
+};
+
+interface IBuildJson {
+	targets: [string, RecipeID][];
+	sources: [RecipeID, string[]][];
+	runtimeSrc: [RecipeID, string[]][];
+}
+
+export class Build {
+	private _runtimeSrcMap = new Map<RecipeID, Set<string>>();
+	private _sources = new Map<RecipeID, Set<string>>();
+	private _targets = new Map<string, RecipeID>();
+
+	constructor(recipes?: RecipeInfo[]) {
+		if (recipes) {
+			for (let id = 0; id < recipes.length; ++id) {
+				const { targets, sources } = recipes[id];
+				this.register(id, targets, sources);
+			}
+		}
+	}
+
+	static async readFile(abs: string): Promise<Build | null> {
+		try {
+			const contents = await readFile(abs, 'utf8');
+			const json = JSON.parse(contents) as IBuildJson;
+			const results = new Build();
+
+			for (const [rel, id] of json.targets) {
+				results._targets.set(rel, id);
+			}
+
+			for (const [recipe, src] of json.runtimeSrc) {
+				results._runtimeSrcMap.set(recipe, new Set<string>(src));
+			}
+
+			for (const [recipe, src] of json.sources) {
+				results._sources.set(recipe, new Set<string>(src));
+			}
+
+			return results;
+		} catch {
+			return null;
+		}
+	}
+
+	async writeFile(abs: string): Promise<void> {
+		const json: IBuildJson = {
+			runtimeSrc: [],
+			targets: [],
+			sources: [],
+		};
+
+		for (const [recipe, src] of this._runtimeSrcMap) {
+			json.runtimeSrc.push([recipe, [...src]]);
+		}
+
+		for (const [recipe, src] of this._sources) {
+			json.sources.push([recipe, [...src]]);
+		}
+
+		for (const [target, recipe] of this._targets) {
+			json.targets.push([target, recipe]);
+		}
+
+		await mkdir(dirname(abs), { recursive: true });
+		await writeFile(abs, JSON.stringify(json), 'utf8');
+	}
+
+	addRuntimeSrc(recipe: RecipeID, srcAbs: Set<string>): void {
+		this._runtimeSrcMap.set(recipe, srcAbs);
+	}
+
+	private register(
+		recipe: RecipeID,
+		targets: IBuildPath[],
+		sources: Path[],
+	): void {
+		for (const t of targets) {
+			this._targets.set(t.rel(), recipe);
+		}
+
+		this._sources.set(recipe, new Set(sources.map((p) => p.rel())));
+	}
+
+	runtimeSrc(target: IBuildPath): Set<string> {
+		const recipe = this._targets.get(target.rel());
+		const src = isRecipeID(recipe) && this._runtimeSrcMap.get(recipe);
+		if (src) return src;
+		return new Set<string>();
+	}
+}
--- src/Cookbook.ts
+++ src/Cookbook.ts
@@ -1,119 +1,17 @@
 import { IRecipe, RecipeBuildArgs, MappedPaths, SourcePaths } from './Recipe';
 import { mapShape } from './SimpleShape';
+import { Mutex, UnlockFunction } from './Mutex';
+import { IBuildPath, BuildPathLike, Path } from './Path';
+import { Build, RecipeID, RecipeInfo, isRecipeID } from './Build';
 
 import { FSWatcher, mkdirSync, statSync } from 'node:fs';
-import { readFile, writeFile } from 'node:fs/promises';
 import { dirname, resolve } from 'node:path';
-import { IBuildPath, BuildPathLike, Path } from './Path';
-import { Mutex, UnlockFunction } from './Mutex';
 import { watch } from 'node:fs';
 import EventEmitter from 'node:events';
-
-type RecipeInfo = {
-	buildAsync(): Promise<boolean>;
-	sources: Path[];
-	targets: IBuildPath[];
-};
 
 export interface ICookbookOpts {
 	buildRoot?: string;
 	srcRoot?: string;
-}
-
-interface IBuildJson {
-	targets: [string, RecipeID][];
-	sources: [RecipeID, string[]][];
-	runtimeSrc: [RecipeID, string[]][];
-}
-
-class Build {
-	private _runtimeSrcMap = new Map<RecipeID, Set<string>>();
-	private _sources = new Map<RecipeID, Set<string>>();
-	private _targets = new Map<string, RecipeID>();
-
-	constructor(recipes?: RecipeInfo[]) {
-		if (recipes) {
-			for (let id = 0; id < recipes.length; ++id) {
-				const { targets, sources } = recipes[id];
-				this.register(id, targets, sources);
-			}
-		}
-	}
-
-	static async readFile(abs: string): Promise<Build | null> {
-		try {
-			const contents = await readFile(abs, 'utf8');
-			const json = JSON.parse(contents) as IBuildJson;
-			const results = new Build();
-
-			for (const [rel, id] of json.targets) {
-				results._targets.set(rel, id);
-			}
-
-			for (const [recipe, src] of json.runtimeSrc) {
-				results._runtimeSrcMap.set(recipe, new Set<string>(src));
-			}
-
-			for (const [recipe, src] of json.sources) {
-				results._sources.set(recipe, new Set<string>(src));
-			}
-
-			return results;
-		} catch {
-			return null;
-		}
-	}
-
-	async writeFile(abs: string): Promise<void> {
-		const json: IBuildJson = {
-			runtimeSrc: [],
-			targets: [],
-			sources: [],
-		};
-
-		for (const [recipe, src] of this._runtimeSrcMap) {
-			json.runtimeSrc.push([recipe, [...src]]);
-		}
-
-		for (const [recipe, src] of this._sources) {
-			json.sources.push([recipe, [...src]]);
-		}
-
-		for (const [target, recipe] of this._targets) {
-			json.targets.push([target, recipe]);
-		}
-
-		mkdirSync(dirname(abs), { recursive: true });
-		await writeFile(abs, JSON.stringify(json), 'utf8');
-	}
-
-	addRuntimeSrc(recipe: RecipeID, srcAbs: Set<string>): void {
-		this._runtimeSrcMap.set(recipe, srcAbs);
-	}
-
-	private register(
-		recipe: RecipeID,
-		targets: IBuildPath[],
-		sources: Path[],
-	): void {
-		for (const t of targets) {
-			this._targets.set(t.rel(), recipe);
-		}
-
-		this._sources.set(recipe, new Set(sources.map((p) => p.rel())));
-	}
-
-	runtimeSrc(target: IBuildPath): Set<string> {
-		const recipe = this._targets.get(target.rel());
-		const src = isRecipeID(recipe) && this._runtimeSrcMap.get(recipe);
-		if (src) return src;
-		return new Set<string>();
-	}
-}
-
-type RecipeID = number;
-function isRecipeID(id: any): id is RecipeID {
-	return typeof id === 'number';
 }
 
 export class Cookbook {

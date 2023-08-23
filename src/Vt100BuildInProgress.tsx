263da550fa429e1cb86467df7c2ee7c57c52ca56
Nicholas Gulachek <gulac004@umn.edu>
2023-08-23 06:57:30 -0500

    some refactoring

--- src/Vt100BuildInProgress.tsx
+++ src/Vt100BuildInProgress.tsx
@@ -6,29 +6,72 @@ import EventEmitter from 'node:events';
 
 interface IBuildDisplayProps {
 	inProgress: InProgressMap;
+	complete: CompleteMap;
 	events: EventEmitter;
 }
 
 function BuildDisplay(props: IBuildDisplayProps) {
-	const { inProgress, events } = props;
+	const { inProgress, complete, events } = props;
 	const tick = useRenderEvent(events);
+	useIntervalMs(25, inProgress.size > 0);
+	const now = performance.now();
 
-	const allComplete = useMemo(() => {
-		for (const entry of inProgress) {
-			if (!entry[1].endTime) return false;
-		}
+	return (
+		<Box flexDirection="column">
+			<CompletedBuilds tick={tick} complete={complete} />
+			<InProgressBuilds now={now} inProgress={inProgress} />
+		</Box>
+	);
+}
 
-		return true;
-	}, [inProgress.size, tick]);
+interface ICompletedBuildsProps {
+	complete: CompleteMap;
+	tick: number;
+}
 
-	const now = useIntervalMs(25, !allComplete);
+function CompletedBuilds(props: ICompletedBuildsProps) {
+	const { complete } = props;
 
 	const times = [];
 	const names = [];
+	for (const [id, info] of complete) {
+		const { name, elapsedTimeMs } = info;
+		const elapsedMs = Math.round(elapsedTimeMs);
+		const elapsedTime = (
+			<Text key={id} color="cyan">
+				[{elapsedMs}ms]
+			</Text>
+		);
+		times.push(elapsedTime);
+		names.push(
+			<Text key={id} wrap="truncate-end">
+				{name}
+			</Text>,
+		);
+	}
+
+	return (
+		<Box flexDirection="row" flexWrap="nowrap" gap={1}>
+			<Box flexDirection="column" width={8}>
+				{times}
+			</Box>
+			<Box flexDirection="column">{names}</Box>
+		</Box>
+	);
+}
+
+interface IInProgressBuildsProps {
+	inProgress: InProgressMap;
+	now: number;
+}
+
+function InProgressBuilds(props: IInProgressBuildsProps) {
+	const { now, inProgress } = props;
+	const times = [];
+	const names = [];
 	for (const [id, info] of inProgress) {
-		const { name, startTime, endTime } = info;
-		const complete = !!endTime;
-		const elapsedMs = Math.round((endTime || now) - startTime);
+		const { name, startTime } = info;
+		const elapsedMs = Math.round(now - startTime);
 		const elapsedTime = (
 			<Text key={id} color="cyan">
 				[{elapsedMs}ms]{' '}
@@ -36,7 +79,7 @@ function BuildDisplay(props: IBuildDisplayProps) {
 		);
 		times.push(elapsedTime);
 		names.push(
-			<Text key={id} dimColor={!complete} wrap="truncate-end">
+			<Text key={id} dimColor wrap="truncate-end">
 				{name}
 			</Text>,
 		);
@@ -56,6 +99,7 @@ export class Vt100BuildInProgress {
 	private _build: IBuild;
 	private _events = new EventEmitter();
 	private _recipesInProgress = new Map<RecipeID, InProgressInfo>();
+	private _completeRecipes = new Map<RecipeID, CompleteInfo>();
 
 	constructor(build: IBuild) {
 		this._build = build;
@@ -69,6 +113,7 @@ export class Vt100BuildInProgress {
 			<BuildDisplay
 				events={this._events}
 				inProgress={this._recipesInProgress}
+				complete={this._completeRecipes}
 			/>,
 		);
 	}
@@ -87,7 +132,13 @@ export class Vt100BuildInProgress {
 	}
 
 	private stopRecipe(id: RecipeID): void {
-		this._recipesInProgress.get(id).endTime = performance.now();
+		const info = this._recipesInProgress.get(id);
+		this._recipesInProgress.delete(id);
+
+		this._completeRecipes.set(id, {
+			name: info.name,
+			elapsedTimeMs: performance.now() - info.startTime,
+		});
 		this.render();
 	}
 
@@ -97,11 +148,16 @@ export class Vt100BuildInProgress {
 }
 
 type InProgressMap = Map<RecipeID, InProgressInfo>;
+type CompleteMap = Map<RecipeID, CompleteInfo>;
 
 type InProgressInfo = {
 	name: string;
 	startTime: number;
-	endTime?: number;
+};
+
+type CompleteInfo = {
+	name: string;
+	elapsedTimeMs: number;
 };
 
 function useRenderEvent(events: EventEmitter): number {

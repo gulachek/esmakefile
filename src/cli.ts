ee53fd6fc4fe6e106303d1fad9f0e3fbf0545ff8
Nicholas Gulachek <gulac004@umn.edu>
2024-05-20 15:57:21 -0500

    Simplify cli and add outdir option
    Remove huge complexity here that was marginally beneficial.
    
    This is a breaking change and is part of reworking config
    mechanism. Opting towards modules defining their own config
    files that can be looked up ad-hoc, which even though at face
    value seems like more work, seems like less work to maintain
    for both esmakefile and users of esmakefile.

--- src/cli.ts
+++ src/cli.ts
@@ -2,216 +2,36 @@ import { Makefile } from './Makefile.js';
 import { Path } from './Path.js';
 import { Vt100BuildInProgress } from './Vt100BuildInProgress.js';
 
-import { Command, Option } from 'commander';
+import { Command } from 'commander';
 
-interface ICliOptionTypeMap {
-	boolean: boolean;
-	string: string;
+export interface IStdBuildOpts {
+	isDevelopment: boolean;
 }
 
-type OptionValues = ICliOptionTypeMap[keyof ICliOptionTypeMap];
+type StdBuildFn = (make: Makefile, opts: IStdBuildOpts) => void;
 
-/**
- * The name of a type that a CLI option value can have
- */
-export type CliOptionValueType = keyof ICliOptionTypeMap;
-
-/**
- * An individual option for the command line
- */
-export interface ICliOptionDescription<
-	OptionValueType extends CliOptionValueType,
-> {
-	/**
-	 * The name of the argument to pass via command line
-	 * @example 'hello-world' to pass as '--hello-world'
-	 */
-	arg: string;
-
-	/**
-	 * The name of the type that the parsed parameter value takes
-	 */
-	type: OptionValueType;
-}
-
-type CliOptionDescriptionOf<T extends CliOptionValueType> = T extends unknown
-	? ICliOptionDescription<T>
-	: never;
-
-/**
- * Union of valid ICliOptionDescription types
- */
-export type CliOptionDescription = CliOptionDescriptionOf<
-	keyof ICliOptionTypeMap
->;
-
-type ValueTypeOfDesc<T extends CliOptionDescription> =
-	T extends CliOptionDescriptionOf<infer TValueType>
-		? ICliOptionTypeMap[TValueType]
-		: never;
-
-/**
- * A group of options to be parsed by the command line
- */
-export type CliOptionGroup = {
-	[key: string]: CliOptionDescription;
-};
-
-/**
- * Options parsed from a CliOptionGroup to configure a build
- */
-export type BuildOptionGroupOf<TCli extends CliOptionGroup> = {
-	[P in keyof TCli]: ValueTypeOfDesc<TCli[P]>;
-};
-
-function makeCliOpts<T extends CliOptionGroup>(opts: T): T {
-	return opts;
-}
-
-const stdOpts = makeCliOpts({
-	isDevelopment: {
-		arg: 'development',
-		type: 'boolean',
-	},
-	sourceRoot: {
-		arg: 'srcdir',
-		type: 'string',
-	},
-});
-
-export type StdBuildOpts = BuildOptionGroupOf<typeof stdOpts>;
-
-/**
- * All groups of options to be parsed by the command line
- */
-export type CliOptions = {
-	[key: string]: CliOptionGroup;
-};
-
-/**
- * All options parsed from CliOptions to configure a build
- */
-export type BuildOptionsOf<TCli extends CliOptions> = {
-	[P in keyof TCli]: BuildOptionGroupOf<TCli[P]>;
-};
-
-type ExtBuildOpts<TCliExt extends CliOptions> = StdBuildOpts & {
-	ext: BuildOptionsOf<TCliExt>;
-};
-
-type StdBuildFn = (make: Makefile, opts: StdBuildOpts) => void;
-type ExtBuildFn<TBuildOpts> = (make: Makefile, opts: TBuildOpts) => void;
-
-type ArgLookup = {
-	extKey?: string;
-	buildKey: string;
-};
-
-function addOptions(
-	cmd: Command,
-	opts: CliOptionGroup,
-	cache: Map<string, ArgLookup>,
-	extKey?: string,
-): void {
-	for (const key in opts) {
-		const { arg, type } = opts[key];
-
-		const lkup = cache.get(arg);
-		if (lkup) {
-			const other = `${lkup.extKey || ''}.${lkup.buildKey}`;
-			const me = `${extKey || ''}.${key}`;
-			throw new Error(
-				`Found conflicting command line arguments '${arg}': Build options "${other}" and "${me}"`,
-			);
-		}
-
-		switch (type) {
-			case 'boolean':
-				cmd.option(`--${arg}`);
-				break;
-			case 'string':
-				cmd.option(`--${arg} <value>`);
-				break;
-			default:
-				throw new Error(`Unknown type '${type}' for CLI arg '${arg}'`);
-		}
-		cache.set(arg, { extKey, buildKey: key });
-	}
-}
-
-function options(cmd: Command): Option[] {
-	return (cmd as unknown as { options: Option[] }).options;
-}
-
-function parseOpts(
-	cmd: Command,
-	args: Map<string, ArgLookup>,
-	opts?: CliOptions,
-): ExtBuildOpts<CliOptions> {
-	const parsedOpts = cmd.opts();
-	const out: Partial<ExtBuildOpts<CliOptions>> = {};
-
-	if (opts) {
-		out.ext = {};
-	}
-
-	for (const opt of options(cmd)) {
-		const arg = opt.name();
-		const parsedValue = parsedOpts[opt.attributeName()];
-
-		type OptionHolder = Record<string, OptionValues>;
-
-		const { extKey, buildKey } = args.get(arg);
-		let optGroup: CliOptionGroup = stdOpts;
-		let obj = out as OptionHolder;
-		if (extKey) {
-			optGroup = opts[extKey];
-			const extHolder = out as Record<string, OptionHolder>;
-			obj = extHolder[extKey] = extHolder[extKey] || {};
-		}
-
-		const cliOpt = optGroup[buildKey];
-
-		switch (cliOpt.type) {
-			case 'string':
-				obj[buildKey] = parsedValue;
-				break;
-			case 'boolean':
-				obj[buildKey] = !!parsedValue;
-				break;
-			default:
-				throw new Error(
-					`Unhandled cli option type '${(cliOpt as { type: unknown }).type}'`,
-				);
-		}
-	}
-
-	return out as ExtBuildOpts<CliOptions>;
-}
-
-export function cli(fn: StdBuildFn): void;
-export function cli<T extends CliOptions>(
-	fn: ExtBuildFn<ExtBuildOpts<T>>,
-	opts: T,
-): void;
-export function cli(
-	fn: StdBuildFn | ExtBuildFn<ExtBuildOpts<CliOptions>>,
-	opts?: CliOptions,
-): void {
+export function cli(fn: StdBuildFn): void {
 	const program = new Command();
-	const args = new Map<string, ArgLookup>();
-	addOptions(program, stdOpts, args);
 
-	if (opts) {
-		for (const extKey in opts) {
-			addOptions(program, opts[extKey], args);
-		}
-	}
+	program.option('--development', 'Specifies this is a development build');
+
+	program.option(
+		'--srcdir <dir>',
+		"Root directory of source files (default is '.')",
+	);
+
+	program.option(
+		'--outdir <dir>',
+		"Root directory of build files (default is './build')",
+	);
 
 	const makeMakefile = () => {
-		const buildOpts = parseOpts(program, args, opts);
-		const make = new Makefile({ srcRoot: buildOpts.sourceRoot });
-		fn(make, buildOpts);
+		const opts = program.opts();
+		const make = new Makefile({
+			srcRoot: opts['srcdir'],
+			buildRoot: opts['outdir'],
+		});
+		fn(make, { isDevelopment: opts['development'] });
 		return make;
 	};
 

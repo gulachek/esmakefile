61d32516f781a8209e081a858eff81910c69f614
Nicholas Gulachek <nsgulachek@gmail.com>
2026-06-08 22:20:32 -0500

    Add a CLI implementation of a LoggerProvider
    This is the beginning of restoring CLI output during program execution

--- src/cli.ts
+++ src/cli.ts
@@ -3,7 +3,20 @@ import { Path, IBuildPath } from './Path.js';
 import { Build } from './Build.js';
 
 import { Command, OptionValues } from 'commander';
+import chalk from 'chalk';
+import {
+	LogLevel,
+	logLevelToStr,
+	LogRecord,
+	Logger,
+	ILoggerProvider,
+	LoggerEventEmitter,
+	GetLoggerOpts,
+	setLoggerProvider,
+} from './logs.js';
+import { fmtElapsedTime } from './fmtElapsedTime.js';
 import { SourceWatcher } from './SourceWatcher.js';
+import EventEmitter from 'node:events';
 
 export interface ICliFnOpts {
 	isDevelopment: boolean;
@@ -50,6 +63,9 @@ export function cli(fn: CliFn): void {
 		.argument('[goal]', 'The goal target to be built')
 		.action(async function (goal?: string) {
 			const opts = this.opts();
+			setLoggerProvider(
+				new CliLoggerProvider(performance.now(), LogLevel.info),
+			);
 			const make = makeMakefile(opts);
 			const goalPath = goal && Path.build(goal);
 			const result = await runBuild(make, goalPath);
@@ -64,8 +80,15 @@ export function cli(fn: CliFn): void {
 		.option('--development', devDesc, true)
 		.action(async function (goal?: string) {
 			const opts = this.opts();
+			const loggerProvider = new CliLoggerProvider(
+				performance.now(),
+				LogLevel.info,
+			);
+			setLoggerProvider(loggerProvider);
 			const make = makeMakefile(opts);
 			const goalPath = goal && Path.build(goal);
+
+			const logger = loggerProvider.getLogger({ name: 'esmakefile.cli.watch' });
 
 			const watcher = new SourceWatcher(make.srcRoot, {
 				debounceMs: 300,
@@ -73,11 +96,13 @@ export function cli(fn: CliFn): void {
 			});
 
 			watcher.on('change', () => {
+				loggerProvider.resetClock();
+				logger.info('Detected change. Restarting update.');
 				runBuild(make, goalPath);
 			});
 
 			watcher.on('unknown', (type: string) => {
-				console.warn(`Unhandled ${SourceWatcher.name} event type '${type}'`);
+				logger.warn(`Unhandled ${SourceWatcher.name} event type '${type}'`);
 			});
 
 			const closeWatcher = () => watcher.close();
@@ -85,6 +110,8 @@ export function cli(fn: CliFn): void {
 			process.stdin.on('close', closeWatcher);
 			process.stdin.on('data', drainStdin);
 
+			logger.info(`Watching '${make.srcRoot}'`);
+			logger.info('Close input stream to stop (usually Ctrl+D)');
 			runBuild(make, goalPath);
 		});
 
@@ -99,4 +126,71 @@ export function cli(fn: CliFn): void {
 		});
 
 	program.parse();
+}
+
+function fmtLogLevel(level: LogLevel): string {
+	// This will throw if level is invalid. Not throwing means
+	// range is valid
+	const s = logLevelToStr(level).padEnd(6);
+
+	if (level < LogLevel.debug)
+		// trace
+		return chalk.bold.magenta(s);
+	else if (level < LogLevel.info)
+		// debug
+		return chalk.bold.cyan(s);
+	else if (level < LogLevel.warn)
+		// info
+		return chalk.bold.white(s);
+	else if (level < LogLevel.error)
+		// warn
+		return chalk.bold.yellow(s);
+	else if (level < LogLevel.fatal)
+		// error
+		return chalk.bold.red(s);
+	// fatal
+	else return chalk.bold.bgRed.white(s);
+}
+
+class CliLoggerProvider implements ILoggerProvider {
+	private tStart: number; // performance.now()
+	private level: LogLevel = LogLevel.info;
+	private evt: LoggerEventEmitter;
+	private logger: Logger;
+
+	constructor(tStart: number, level: LogLevel) {
+		this.level = level;
+		this.evt = new EventEmitter() as LoggerEventEmitter;
+		this.evt.on('log', (r) => this.log(r));
+		this.logger = this.getLogger({ name: 'esmakefile.CliLoggerProvider' });
+		this.resetClock(tStart);
+	}
+
+	resetClock(tStart?: number): void {
+		this.tStart = tStart || performance.now();
+		const dStart = new Date(performance.timeOrigin + this.tStart);
+		this.logger.debug(
+			`Set ${CliLoggerProvider.name} clock to ${dStart.toISOString()}`,
+		);
+	}
+
+	getLogger(opts: GetLoggerOpts): Logger {
+		const l = new Logger(this.evt, opts);
+		l.setLogLevel(this.level);
+		return l;
+	}
+
+	private log(l: LogRecord): void {
+		if (l.level < this.level) return;
+
+		const { timeStamp, body, level } = l;
+		const tStr =
+			'[' + chalk.cyan(fmtElapsedTime(timeStamp - this.tStart)) + ']';
+		const levelStr = fmtLogLevel(level);
+		console.log(`${tStr} ${levelStr}`, trimConsoleMsg(body));
+	}
+}
+
+function trimConsoleMsg(msg: string): string {
+	return msg.trim().replaceAll(/\s+/g, ' ');
 }

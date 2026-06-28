6c15ca24f5d0bfec819b196390ac2178b4f1b650
Nicholas Gulachek <nsgulachek@gmail.com>
2026-06-28 15:00:05 -0500

    Add option to debug-spec.sh to target specific file

--- debug-spec.sh
+++ debug-spec.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 
-# Run in current directory. Connect via chrome and add debugger statement to line that's being hit.
-node --inspect-brk ./node_modules/mocha/bin/mocha.js
+script=$(realpath "$0")
+rootDir=$(dirname "$script")
+
+spec="$1"
+
+if [ -z "$spec" ]; then
+	# Run in current directory. Connect via chrome and add debugger statement to line that's being hit.
+	node --inspect-brk ./node_modules/mocha/bin/mocha.js
+	exit $?
+fi
+
+# Confirm this is a .js or .ts spec file
+if [ ! -r "$spec" ]; then
+	echo "File '$spec' is not a readable file" >&2
+	exit 1
+fi
+
+if [[ "$spec" != */spec/*.[jt]s ]]; then
+	echo "File '$spec' is not a mocha test" >&2
+	exit 1
+fi
+
+spec=$(realpath "$spec")
+
+# Transform ".ts" to corresponding ".js"
+if [ "${spec:(-3)}" = ".ts" ]; then
+	srcRoot="$rootDir/src/"
+	distRoot="$rootDir/dist/"
+	specRel="${spec#$srcRoot}"
+	spec="${distRoot}${specRel%%.ts}.js"
+fi
+
+node --inspect-brk ./node_modules/mocha/bin/mocha.js --config=<(echo '{}') "$spec"

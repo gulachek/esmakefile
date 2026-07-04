e4ff6479f114683a590d0dcdadba788b0472a5a0
Nicholas Gulachek <nsgulachek@gmail.com>
2026-07-04 11:06:03 -0500

    Change make-cli.ts to a standalone executable
    This change allows loading a Makefile JS module from the directory given
    in the CLI. It has quite a bit of diagnostics logged under `--debug` or
    `--trace`.
    
    The paradigm for writing a Makefile is now different. Instead of making
    a script that is directly passed to node, the make-cli.ts script is the
    top level script given to node, and it imports a user-written module
    which defines the build system. The module is looked up by well-known
    names, in order:
    
    - esmakefile.js
    - makefile.js
    - Makefile.js
    
    mjs and cjs extensions are also accepted.

--- src/make-cli.ts
+++ src/make-cli.ts
@@ -4,7 +4,7 @@ import { InMemoryArtifactStore } from './InMemoryArtifactStore.js';
 import { MakeProgram } from './MakeProgram.js';
 
 import { Command, OptionValues } from 'commander';
-import { LogLevel, setLoggerProvider } from './logs.js';
+import { LogLevel, setLoggerProvider, Logger } from './logs.js';
 import { SourceWatcher } from './SourceWatcher.js';
 import {
 	ATTR_SERVICE_NAME,
@@ -13,6 +13,9 @@ import {
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { CliLoggerProvider } from './CliLoggerProvider.js';
+import { resolve } from 'node:path';
+import { Stats, statSync } from 'node:fs';
+import { pathToFileURL } from 'node:url';
 
 const artifactImpl = new InMemoryArtifactStore();
 setArtifactStoreImpl(artifactImpl);
@@ -30,125 +33,235 @@ const sdk = new NodeSDK({
 });
 sdk.start();
 
-export function cli(fn: MakefileFn): void {
-	const program = new Command();
-	const logger = loggerProvider.getLogger({ name: 'esmakefile.cli' });
+const program = new Command();
+const logger = loggerProvider.getLogger({ name: 'esmakefile.cli' });
 
-	const devDesc = 'Specifies this is a development build';
-	program.option('--development', devDesc, false);
+const devDesc = 'Specifies this is a development build';
+program.option('--development', devDesc, false);
 
-	program.option(
-		'-C, --directory <dir>',
-		"Root directory of the build system (default is '.')",
-	);
+program.option(
+	'-C, --directory <dir>',
+	"Root directory of the build system (default is '.')",
+);
 
-	program.option('--trace', 'Sets the log level to "trace"', false);
-	program.option('-v, --debug', 'Sets the log level to "debug"', false);
+program.option('--trace', 'Sets the log level to "trace"', false);
+program.option('-v, --debug', 'Sets the log level to "debug"', false);
 
-	const makeProgram = async (cmdOpts: OptionValues) => {
-		const opts = { ...program.opts(), ...cmdOpts };
-		return MakeProgram.parse(fn, {
-			rootDir: opts['directory'],
+const makeProgram = async (cmdOpts: OptionValues) => {
+	const opts = { ...program.opts(), ...cmdOpts };
+
+	let mod: IMakefileModule | null;
+	try {
+		mod = await loadMakefileModule(opts['directory'] || '.', logger);
+	} catch (exception) {
+		logger.fatal({
+			body: 'Scan for Makefile threw an exception',
+			exception,
 		});
-	};
+		process.exit(1);
+	}
 
-	const parseLogLevel = (cmdOpts: OptionValues): LogLevel => {
-		const opts = { ...program.opts(), ...cmdOpts };
+	if (!mod) {
+		logger.fatal('Failed to load a Makefile module');
+		process.exit(1);
+	}
 
-		const i = LogLevel.info;
-		if (!opts) return i;
-		if (typeof opts !== 'object') return i;
-		if (opts['trace']) return LogLevel.trace;
-		if (opts['debug']) return LogLevel.debug;
-		return i;
-	};
+	return MakeProgram.parse(mod.main, {
+		rootDir: opts['directory'],
+	});
+};
 
-	program
-		.command('build', { isDefault: true })
-		.description('Build a specified target')
-		.argument('[goal]', 'The goal target to be built')
-		.action(async function (goal?: string) {
-			const opts = this.opts();
-			loggerProvider.setLogLevel(parseLogLevel(opts));
-			loggerProvider.resume();
+const parseLogLevel = (cmdOpts: OptionValues): LogLevel => {
+	const opts = { ...program.opts(), ...cmdOpts };
 
-			const make = await makeProgram(opts);
+	const i = LogLevel.info;
+	if (!opts) return i;
+	if (typeof opts !== 'object') return i;
+	if (opts['trace']) return LogLevel.trace;
+	if (opts['debug']) return LogLevel.debug;
+	return i;
+};
 
-			if (!make) {
-				logger.fatal({
-					body: 'Failed to create Makefile',
-				});
-				process.exit(1);
-			}
+program
+	.command('build', { isDefault: true })
+	.description('Build a specified target')
+	.argument('[goal]', 'The goal target to be built')
+	.action(async function (goal?: string) {
+		const opts = this.opts();
+		loggerProvider.setLogLevel(parseLogLevel(opts));
+		loggerProvider.resume();
 
-			const result = await make.update(goal);
+		const make = await makeProgram(opts);
 
-			process.exit(result ? 0 : 1);
+		if (!make) {
+			logger.fatal({
+				body: 'Failed to create Makefile',
+			});
+			process.exit(1);
+		}
+
+		const result = await make.update(goal);
+
+		process.exit(result ? 0 : 1);
+	});
+
+program
+	.command('watch')
+	.description('Rebuild top level targets when a source file changes')
+	.argument('[goal]', 'The goal target to be built')
+	.option('--development', devDesc, true)
+	.action(async function (goal?: string) {
+		const opts = this.opts();
+		loggerProvider.setLogLevel(parseLogLevel(opts));
+		loggerProvider.resume();
+
+		const make = await makeProgram(opts);
+
+		if (!make) {
+			logger.fatal({
+				body: 'Failed to create Makefile',
+			});
+			process.exit(1);
+		}
+
+		const watcher = new SourceWatcher(make.rootDir, {
+			debounceMs: 300,
+			// TODO - have a way to ignore a directory while watching
+			excludeDir: '__TODO__',
 		});
 
-	program
-		.command('watch')
-		.description('Rebuild top level targets when a source file changes')
-		.argument('[goal]', 'The goal target to be built')
-		.option('--development', devDesc, true)
-		.action(async function (goal?: string) {
-			const opts = this.opts();
-			loggerProvider.setLogLevel(parseLogLevel(opts));
-			loggerProvider.resume();
-
-			const make = await makeProgram(opts);
-
-			if (!make) {
-				logger.fatal({
-					body: 'Failed to create Makefile',
-				});
-				process.exit(1);
-			}
-
-			const watcher = new SourceWatcher(make.rootDir, {
-				debounceMs: 300,
-				// TODO - have a way to ignore a directory while watching
-				excludeDir: '__TODO__',
-			});
-
-			watcher.on('change', () => {
-				loggerProvider.resetClock();
-				logger.info('Detected change. Restarting update.');
-				make.update(goal);
-			});
-
-			watcher.on('unknown', (type: string) => {
-				logger.warn(`Unhandled ${SourceWatcher.name} event type '${type}'`);
-			});
-
-			const closeWatcher = () => watcher.close();
-			const drainStdin = () => process.stdin.read();
-			process.stdin.on('close', closeWatcher);
-			process.stdin.on('data', drainStdin);
-
-			logger.info(`Watching '${make.rootDir}'`);
-			logger.info('Close input stream to stop (usually Ctrl+D)');
+		watcher.on('change', () => {
+			loggerProvider.resetClock();
+			logger.info('Detected change. Restarting update.');
 			make.update(goal);
 		});
 
-	program
-		.command('list')
-		.description('List all targets')
-		.action(async function () {
-			const make = await makeProgram(this.opts());
-			if (!make) {
-				// TODO - make this command work with logs
-				loggerProvider.resume();
-				logger.fatal({
-					body: 'Failed to create Makefile',
-				});
-				process.exit(1);
-			}
-
-			for (const t of make.targets()) {
-				console.log(t);
-			}
+		watcher.on('unknown', (type: string) => {
+			logger.warn(`Unhandled ${SourceWatcher.name} event type '${type}'`);
 		});
 
-	program.parse();
+		const closeWatcher = () => watcher.close();
+		const drainStdin = () => process.stdin.read();
+		process.stdin.on('close', closeWatcher);
+		process.stdin.on('data', drainStdin);
+
+		logger.info(`Watching '${make.rootDir}'`);
+		logger.info('Close input stream to stop (usually Ctrl+D)');
+		make.update(goal);
+	});
+
+program
+	.command('list')
+	.description('List all targets')
+	.action(async function () {
+		const make = await makeProgram(this.opts());
+		if (!make) {
+			// TODO - make this command work with logs
+			loggerProvider.resume();
+			logger.fatal({
+				body: 'Failed to create Makefile',
+			});
+			process.exit(1);
+		}
+
+		for (const t of make.targets()) {
+			console.log(t);
+		}
+	});
+
+program.parseAsync();
+
+interface IMakefileModule {
+	main: MakefileFn;
+}
+
+async function loadMakefileModule(
+	rootDir: string,
+	logger: Logger,
+): Promise<IMakefileModule | null> {
+	const trace = logger.enabled({ level: LogLevel.trace });
+	const debug = logger.enabled({ level: LogLevel.debug });
+	if (trace) {
+		logger.trace(`Scanning directory '${rootDir}' for Makefile`);
+	}
+	const basenames = ['esmakefile', 'makefile', 'Makefile'];
+	const exts = ['.mjs', '.cjs', '.js'];
+
+	for (const b of basenames) {
+		for (const e of exts) {
+			const f = resolve(rootDir, b + e);
+			if (trace) {
+				logger.trace(`Trying '${f}' as Makefile`);
+			}
+
+			let st: Stats;
+			try {
+				st = statSync(f, { throwIfNoEntry: false });
+			} catch (ex) {
+				logger.warn({
+					body: `Failed to statSync('${f}')`,
+					exception: ex,
+				});
+			}
+
+			if (!st) {
+				if (trace) {
+					logger.trace(`Dismissing '${f}' because statSync() failed`);
+				}
+				continue;
+			}
+
+			const url = pathToFileURL(f).href;
+			if (debug) {
+				logger.debug(`Attempting import('${url}')`);
+			}
+
+			let mod: unknown;
+			try {
+				mod = await import(url);
+			} catch (exception) {
+				logger.error({
+					body: `Failed to import('${url}')`,
+					exception,
+				});
+			}
+
+			if (!mod) {
+				if (trace) {
+					logger.trace(`Dismissing '${f}' because import() failed`);
+				}
+				continue;
+			}
+
+			if (debug) {
+				logger.debug(`import('${url}') succeeded`);
+			}
+
+			if (typeof mod !== 'object') {
+				logger.error('Result of import() is not of type object');
+				continue;
+			}
+
+			if ('default' in mod && typeof mod.default === 'function') {
+				if (debug) {
+					logger.debug(
+						`Module has default export of function type named '${mod.default.name}'. Considering successful load.`,
+					);
+				}
+				return { main: mod.default as MakefileFn };
+			}
+
+			if ('main' in mod && typeof mod.main === 'function') {
+				if (debug) {
+					logger.debug(
+						`Module has export of function type named 'main'. Considering successful load.`,
+					);
+				}
+				return { main: mod.main as MakefileFn };
+			}
+		}
+	}
+
+	logger.error('Did not successfully load any candidates as Makefile');
+	return null;
 }

4ee9bf5defb70e48c225fb865da7652ca7f57388
Nicholas Gulachek <nsgulachek@gmail.com>
2026-07-31 17:06:59 -0500

    Allow unused variables starting with `_`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,16 @@ import js from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
+/** @import {Linter} from 'eslint' */
+/** @type {Linter.RuleSeverityAndOptions} */
+const noUnusedVars = [
+	'warn',
+	{
+		argsIgnorePattern: '^_',
+		varsIgnorePattern: '^_',
+	},
+];
+
 export default defineConfig([
 	globalIgnores([
 		'node_modules/',
@@ -15,9 +25,15 @@ export default defineConfig([
 	{
 		files: ['**/*.{js,mjs,cjs}'],
 		extends: [js.configs.recommended],
+		rules: {
+			'no-unused-vars': noUnusedVars,
+		},
 	},
 	{
 		files: ['**/*.{ts,mts,cts}'],
 		extends: [js.configs.recommended, tseslint.configs.recommended],
+		rules: {
+			'@typescript-eslint/no-unused-vars': noUnusedVars,
+		},
 	},
 ]);
